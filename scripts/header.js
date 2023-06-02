8e4924c7d2e785215c51ee3dee3d9d3e8815a6c7
Lautaro <lautaro.romczyk@gmail.com>
2023-06-01 21:33:10 -0300

    Agregar sub menu

--- scripts/header.js
+++ scripts/header.js
@@ -1,11 +1,28 @@
 const botonMenu = document.querySelector('.menu-btn');
 const menu = document.querySelector('.lista-navegacion');
+const subMenuHover = document.querySelector('#sub-nav-hover');
+const subMenuFundacion = document.querySelector('.sub-nav');
+const contenedorPrincipal = document.querySelector('.contenedor-principal');
+
+function toggleMenu() {
+  menu.classList.toggle('ocultar');
+}
+
+function mostrarSubMenu() {
+  subMenuFundacion.classList.remove('ocultar');
+}
+
+function ocultarSubMenu() {
+  subMenuFundacion.classList.add('ocultar');
+}
 
 function ocultarHeaderEnPantallaChica() {
   if (window.innerWidth < 800) {
     menu.classList.add("ocultar");
+    mostrarSubMenu();
   } else {
     menu.classList.remove("ocultar");
+    ocultarSubMenu();
   }
 }
 
@@ -13,9 +30,7 @@ function ocultarHeaderEnPantallaChica() {
 window.addEventListener("DOMContentLoaded", ocultarHeaderEnPantallaChica);
 window.addEventListener("resize", ocultarHeaderEnPantallaChica);
 
-function toggleMenu() {
-  menu.classList.toggle('ocultar');
-}
-
 // Mostrar u ocultar el menú cuando se haga clic en el botón
 botonMenu.addEventListener('click', toggleMenu);
+subMenuHover.addEventListener('mouseover', mostrarSubMenu);
+contenedorPrincipal.addEventListener('mouseover', ocultarSubMenu);
